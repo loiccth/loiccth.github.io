29a2f312902fd8198319a1b3587ba8452dc1a09d
Loïc Cth <50406142+loiccth@users.noreply.github.com>
2020-10-27 06:25:39 +0400

    Updated website

---
+++ precache-manifest.8af0be3dac37129edd3dd197767f7c4a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdacf749e0ca2ce0d6a069012d6c7ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef23d9e95a516440695e",
+    "url": "./static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "cd1305f5a2bc8c37c596",
+    "url": "./static/css/main.1204016b.chunk.css"
+  },
+  {
+    "revision": "ef23d9e95a516440695e",
+    "url": "./static/js/2.fa57e0e3.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "./static/js/2.fa57e0e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd1305f5a2bc8c37c596",
+    "url": "./static/js/main.3d6ff2cb.chunk.js"
+  },
+  {
+    "revision": "fd2b481d26bacea8939a",
+    "url": "./static/js/runtime-main.9f96b81b.js"
+  },
+  {
+    "revision": "3dd1c280c0e737dfe7170213e27699e2",
+    "url": "./static/media/covid.3dd1c280.jpg"
+  },
+  {
+    "revision": "fd150a200b5eecce93aae7a9a5458ccd",
+    "url": "./static/media/covid2.fd150a20.jpg"
+  },
+  {
+    "revision": "a2be179c5f856c3ce502d7fd50e6c81e",
+    "url": "./static/media/covid3.a2be179c.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "./static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "./static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "./static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "./static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "./static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "./static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "./static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "./static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "35f8e48e8c84f40c77d38c997e26cdc5",
+    "url": "./static/media/mario.35f8e48e.png"
+  },
+  {
+    "revision": "f21b3bed219160fa1be9c3c15c2c21cc",
+    "url": "./static/media/mario2.f21b3bed.png"
+  },
+  {
+    "revision": "b5b11b3113e15d5ad797b93eee4e67c0",
+    "url": "./static/media/mario3.b5b11b31.png"
+  },
+  {
+    "revision": "d3c35d0a9263cb5d454dcaebfcd04a66",
+    "url": "./static/media/wip.d3c35d0a.jpg"
+  }
+]);
